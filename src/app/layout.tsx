e5cbdab4d659e8746e12904729b08139138bf186
Dominik Kanjuh <dominik.kanjuh@outlook.com>
2024-05-19 22:59:52 +0200

    meta data and og tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,29 @@ import '../styles/globals.css'
 export const metadata: Metadata = {
   title: 'Game X.PRESS',
   description: 'Studentsko natjecanje u izradi video igara',
+  openGraph: {
+    title: 'Game X.PRESS',
+    description: 'Studentsko natjecanje u izradi video igara',
+    url: 'https://game-xpress.xfer.hr/',
+    type: 'website',
+    locale: 'hr_HR',
+    siteName: 'Game X.PRESS',
+    images: [
+      {
+        url: 'https://game-xpress.xfer.hr/static/images/hero-image.png',
+        width: 1792,
+        height: 1024,
+        alt: 'Game X.PRESS',
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Game X.PRESS',
+    description: 'Studentsko natjecanje u izradi video igara',
+    creator: '@x.fer',
+    images: ['https://game-xpress.xfer.hr/static/images/hero-image.png'],
+  },
 }
 
 export default function RootLayout({
