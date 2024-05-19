23a6b1fb6b2ca4c0d67a2357ace1c7f36a865a70
Dominik Kanjuh <dominik.kanjuh@outlook.com>
2024-05-20 01:16:56 +0200

    fix wrong br in li

--- src/components/pages/Step3.tsx
+++ src/components/pages/Step3.tsx
@@ -8,11 +8,11 @@ const Step3 = () => {
         <ul className="mt-6 rounded-lg bg-white p-4 text-xl md:text-2xl lg:text-3xl">
           <li>🫂 Veličina ekipe → 3 osobe</li>
           <li>🔟 Broj ekipa → ograničen na 10 ekipa (Prijavi se što prije)</li>
-          <br />
-          <li>🏰 Žanr → proizvoljan (Adventure, Puzzle, Simulation, FPS, MOBA...)</li>
+
+          <li className="mt-6">🏰 Žanr → proizvoljan (Adventure, Puzzle, Simulation, FPS, MOBA...)</li>
           <li>👾 Stil → što god vas veseli (Pixel Art, 2D, 3D...)</li>
-          <br />
-          <li>⚙️ Tehnologija → bilo koja (Unity, UE5, Godot, PyGame...)</li>
+
+          <li className="mt-6">⚙️ Tehnologija → bilo koja (Unity, UE5, Godot, PyGame...)</li>
           <li>🤫 Tema → ista za sve i obavezna, ali tajna do početka natjecanja </li>
         </ul>
       </div>
