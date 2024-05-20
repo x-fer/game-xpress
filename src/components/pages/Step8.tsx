5066770a2858760e640fb672b1b9a0e76825e19f
Dominik Kanjuh <dominik.kanjuh@outlook.com>
2024-05-20 02:36:34 +0200

    23:59

--- src/components/pages/Step8.tsx
+++ src/components/pages/Step8.tsx
@@ -9,7 +9,7 @@ const Step8 = () => {
         <div className="flex h-full w-full flex-col items-center justify-center gap-10 p-4 sm:p-6 md:p-8  lg:p-10">
           <div className="flex w-full flex-col items-center justify-center md:flex-row md:items-center md:justify-between">
             <h2 className="mb-8 text-center text-2xl font-extrabold  text-black md:mb-0 md:mr-10 lg:text-3xl xl:text-4xl">
-              Prijave se zatvaraju 2.6.2024. u 24:00
+              Prijave se zatvaraju 2.6.2024. u 23:59
             </h2>
             <Countdown />
           </div>
