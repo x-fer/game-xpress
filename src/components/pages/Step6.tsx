5a8d39c41ae6370c1dee2a3c5b07005da8e5de89
Dominik Kanjuh <dominik.kanjuh@outlook.com>
2024-05-20 03:23:25 +0200

    contrast on xfer page

--- src/components/pages/Step6.tsx
+++ src/components/pages/Step6.tsx
@@ -6,7 +6,7 @@ const Step6 = () => {
     <StepContainer className="flex-1 bg-turquoise pb-14">
       <div className="h-full w-full flex-col items-start p-6 sm:p-8 md:p-10 lg:p-12">
         <h2 className="text-5xl font-bold text-purple md:text-6xl lg:text-7xl">Organizatori #1</h2>
-        <p className="mt-6 text-lg text-gray md:text-2xl lg:text-3xl">
+        <p className="mt-6 text-lg text-purple md:text-2xl lg:text-3xl">
           <a
             href="https://xfer.hr/"
             target="_blank"
@@ -47,7 +47,7 @@ const Step6 = () => {
           . Članovi udruge su bivši natjecatelji i olimpijci, a udruga trenutno ima oko 20 aktivnih i preko 50 počasnih
           članova.
         </p>
-        <div className="mt-4 flex flex-row text-gray  md:flex-col md:gap-3">
+        <div className="mt-4 flex flex-row text-purple  md:flex-col md:gap-3">
           <a
             href="https://www.facebook.com/xferhr"
             target="_blank"
