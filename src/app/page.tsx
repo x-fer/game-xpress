43a4a6aff9884ad4e23fe4e6507ad9e737ef3ff4
Dominik Kanjuh <dominik.kanjuh@outlook.com>
2024-06-05 02:35:10 +0200

    Comment the rest of the code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,20 @@
 'use client'
 
-import { ButtonSignUp } from '@/components'
+// import { ButtonSignUp } from '@/components'
 import { Partners1, Step1, Step2, Step3, Step4, Step5, Step6, Step7, Step8 } from '@/components/pages'
-import { useScroll, useTransform } from 'framer-motion'
+// import { useScroll, useTransform } from 'framer-motion'
 import { ElementRef, useRef } from 'react'
 
 export default function Home() {
   const ref = useRef<ElementRef<'div'>>(null)
-  const { scrollYProgress } = useScroll({
-    target: ref,
-    offset: ['start end', 'end end'],
-  })
+  // const { scrollYProgress } = useScroll({
+  //   target: ref,
+  //   offset: ['start end', 'end end'],
+  // })
 
-  const scale = useTransform(scrollYProgress, [0, 1], [0.5, 1])
-  const displayStepCounter = useTransform(scrollYProgress, [0, 1], [1, 7])
-  const display = useTransform(displayStepCounter, (value) => (value <= 2.5 ? 'none' : 'block'))
+  // const scale = useTransform(scrollYProgress, [0, 1], [0.5, 1])
+  // const displayStepCounter = useTransform(scrollYProgress, [0, 1], [1, 7])
+  // const display = useTransform(displayStepCounter, (value) => (value <= 2.5 ? 'none' : 'block'))
 
   return (
     <main ref={ref} className="mt-0 flex w-full flex-col items-center justify-between">
