cbf267fda95faa80e6384fd824bbb8aa6d772cab
Dominik Kanjuh <dominik.kanjuh@outlook.com>
2025-05-20 16:56:11 +0200

    fix cover image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,27 +9,29 @@ const pixelFont = localFont({
 	display: 'swap'
 });
 
+const baseUrl = process.env.NEXT_PUBLIC_URL || 'https://game-xpress.xfer.hr';
+
 export const metadata: Metadata = {
 	title: 'Game X.Press',
 	description:
 		'Game jam by X.FER | May 31-June 1 (24h) @ Algebra (Gradišćanska 24) | Any genre, style or technology allowed | Anyone 18+ can compete (students prioritized)',
 	icons: [
 		{ rel: 'icon', url: '/assets/logo.png', sizes: '32x32' },
+		{ rel: 'icon', url: '/assets/logo.png', sizes: '16x16' },
 		{ rel: 'apple-touch-icon', url: '/assets/logo.png', sizes: '180x180' },
 		{ rel: 'icon', url: '/assets/logo.png', sizes: '192x192' },
-		{ rel: 'icon', url: '/assets/logo.png', sizes: '16x16' },
 		{ rel: 'icon', url: '/assets/logo.png', sizes: '512x512' }
 	],
 	openGraph: {
 		title: 'Game X.Press',
 		description:
 			'Game jam by X.FER | May 31-June 1 (24h) @ Algebra (Gradišćanska 24) | Any genre, style or technology allowed | Anyone 18+ can compete (students prioritized)',
-		url: 'https://game-xpress.xfer.hr/',
+		url: baseUrl,
 		type: 'website',
 		siteName: 'game-xpress.xfer.hr',
 		images: [
 			{
-				url: '/assets/cover.png',
+				url: `${baseUrl}/assets/cover.png`,
 				width: 1200,
 				height: 630,
 				alt: 'Game X.Press 2025 Cover Image'
