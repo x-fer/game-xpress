a327e2511cfbcc642869b8b870e665375ae30f35
Dominik Kanjuh <dominik.kanjuh@outlook.com>
2025-05-20 16:50:51 +0200

    add favicon and cover

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,13 @@ export const metadata: Metadata = {
 	title: 'Game X.Press',
 	description:
 		'Game jam by X.FER | May 31-June 1 (24h) @ Algebra (Gradišćanska 24) | Any genre, style or technology allowed | Anyone 18+ can compete (students prioritized)',
-	icons: [{ rel: 'icon', url: '/favicon.png' }],
+	icons: [
+		{ rel: 'icon', url: '/assets/logo.png', sizes: '32x32' },
+		{ rel: 'apple-touch-icon', url: '/assets/logo.png', sizes: '180x180' },
+		{ rel: 'icon', url: '/assets/logo.png', sizes: '192x192' },
+		{ rel: 'icon', url: '/assets/logo.png', sizes: '16x16' },
+		{ rel: 'icon', url: '/assets/logo.png', sizes: '512x512' }
+	],
 	openGraph: {
 		title: 'Game X.Press',
 		description:
@@ -23,12 +29,20 @@ export const metadata: Metadata = {
 		siteName: 'game-xpress.xfer.hr',
 		images: [
 			{
-				url: 'https://game-xpress.xfer.hr/cover.png',
+				url: '/assets/cover.png',
 				width: 1200,
-				height: 630
+				height: 630,
+				alt: 'Game X.Press 2025 Cover Image'
 			}
 		],
 		locale: 'hr-HR'
+	},
+	twitter: {
+		card: 'summary_large_image',
+		title: 'Game X.Press',
+		description:
+			'Game jam by X.FER | May 31-June 1 (24h) @ Algebra (Gradišćanska 24) | Any genre, style or technology allowed | Anyone 18+ can compete (students prioritized)',
+		images: ['/assets/cover.png']
 	}
 };
 
